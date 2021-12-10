3e63d0ed0ed35a228bfe7ad3abf1de2666e344a9
JasonMSchley <92404151+JasonMSchley@users.noreply.github.com>
2021-12-09 18:53:18 -0800

    Delete project-two/force-app/main/default/aura/ProjectTwoBooksRecommendations directory

--- project-two/force-app/main/default/aura/ProjectTwoBooksRecommendations/ProjectTwoBooksRecommendationsController.js
+++
@@ -1,9 +0,0 @@
-({
-  doInit: function(component, event, helper) {
-    var action = component.get("c.getBookRecommendations");
-    action.setCallback(this, function(data) {
-      component.set("v.Recommendations", data.getReturnValue());
-      console.log(data.getReturnValue()); });
-    $A.enqueueAction(action);
-  }
-})
